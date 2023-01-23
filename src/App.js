44be0c87f5606432d8330d6eed5fd99aec8423ad
bappasahabapi2@gmail.com <bappasahabapi2@gmail.com>
2023-01-23 22:16:25 +0600

    sign out

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
-import { getAuth, GoogleAuthProvider, signInWithPopup} from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup,signOut} from "firebase/auth";
 import app from './firebase/firebase.init';
+import { useState } from 'react';
 
 const auth =getAuth(app);
 
 function App() {
 
+  const [user, setUser]=useState({});
   const provider = new GoogleAuthProvider()
 
   const handleGoogleSingIn =()=>{
@@ -14,6 +16,7 @@ function App() {
       .then(result =>{
         const user =result.user;
         console.log(user)
+        setUser(user)
       })
       .catch(error =>{
         console.error('Error is: ',error);
@@ -21,10 +24,30 @@ function App() {
 
   };
 
+  const handleSignOut =()=>{
+    signOut(auth).then(() => {
+      setUser({});
+      // Sign-out successful.
+    }).catch((error) => {
+      setUser({});
+      // An error happened.
+    });
+  }
+
  
   return (
     <div className="App">
      <button style={{fontSize:'30px',height:'100px', width:'300px',marginTop:'500px'}} onClick={handleGoogleSingIn}>🟢Google Sign In </button>
+     <button onClick={handleSignOut} style={{marginTop:'50px',width:'200px', padding:'10px',fontSize:'20px'}} >Sign Out ❌</button>
+     <div>
+      
+     <p>🧝User Name:👉 <b>{user.displayName}</b></p>
+     <span>Email 👉 <b>{user.email}</b></span>
+     <p>uid 👉 <b>{user.uid}</b></p>
+     
+     <img style={{height:'100px'}} src={user?.photoURL} alt="image"/>
+     
+     </div>
     </div>
   );
 }
